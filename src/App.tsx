76dfa90bd1a640340c89f73fe7d0404e744a00e6
vidman22 <29338715+vidman22@users.noreply.github.com>
2022-07-04 15:44:04 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState('');
+    return (
+        <div className='bg-slate-50 w-full h-screen'>
+            <header className='relative z-50 bg-white py-3 min-w-full flex items-center justify-between shadow-md'>
+                <input
+                    onChange={(e) => setValue(e.target.value)}
+                    value={value}
+                    placeholder='Search'
+                />
+            </header>
+            <div className='text-slate-500'>
+                
+            </div>
+        </div>
+    );
 }
 
 export default App;
